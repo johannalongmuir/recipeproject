0a48c230b1c2b52c2197e8a0f653ae6245952b95
Johanna Longmuir <johannalongmuir@gmail.com>
2020-03-11 15:37:40 +0000

    Initial Commit

--- src/main/java/com/manchesterdigital/RecipeprojectApplication.java
+++ src/main/java/com/manchesterdigital/RecipeprojectApplication.java
@@ -1,4 +1,4 @@
-package com.manchesterdigital.recipeproject;
+package com.manchesterdigital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
