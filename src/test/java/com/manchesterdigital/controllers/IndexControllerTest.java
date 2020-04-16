e5d1bf1cab7aa650a727c25c31ed6e6fbf79e8a0
Johanna Longmuir <johannalongmuir@gmail.com>
2020-04-16 14:25:20 +0100

    Add MockMvc test to index controller tests

--- src/test/java/com/manchesterdigital/controllers/IndexControllerTest.java
+++ src/test/java/com/manchesterdigital/controllers/IndexControllerTest.java
@@ -1,11 +1,21 @@
 package com.manchesterdigital.controllers;
 
+import com.manchesterdigital.domain.Recipe;
 import com.manchesterdigital.services.RecipeService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -21,12 +31,84 @@ class IndexControllerTest {
     Model modelMock = mock(Model.class);
     IndexController indexController = new IndexController(recipeServiceMock);
 
+    ArgumentCaptor<List<Recipe>> recipeListArgumentCapture;
+    List<Recipe> recipeList;
+    String viewName;
+
 
     @Test
-    void getIndexPage() {
-        String viewName = indexController.getIndexPage(modelMock);
-        assertEquals("index", viewName);
-        verify(recipeServiceMock, times(1)).getRecipes();
+    void mockMVC() throws Exception {
+        // very much a unit test
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+    }
+
+    @Test
+    void when_get_index_page_return_index_view() {
+        whenGetIndexPage();
+        thenReturnIndexViewName();
+    }
+
+    @Test
+    void when_get_index_page_methods_called_once() {
+        whenGetIndexPage();
+        thenGetRecipesIsCalledOnce();
+        thenAddAttributeForModelIsCalledOnce();
+    }
+
+    @Test
+    void when_get_index_page_parameters_for_model_are_expected() {
+        givenListWithTwoRecipes();
+        givenGetRecipesReturnsListWithTwoRecipes();
+        whenGetIndexPage();
+        whenCheckingRecipeListReturned();
+        thenArgumentCaptureContainsTwoRecipes();
+    }
+
+
+
+
+
+
+    private void givenGetRecipesReturnsListWithTwoRecipes() {
+        when(recipeServiceMock.getRecipes()).thenReturn(recipeList);
+    }
+
+
+    private void givenListWithTwoRecipes() {
+        recipeList = new ArrayList<>();
+        recipeList.add(Recipe.builder().description("Recipe One").build());
+        recipeList.add(Recipe.builder().description("Recipe Two").build());
+    }
+
+    private void whenGetIndexPage() {
+        viewName = indexController.getIndexPage(modelMock);
+    }
+
+    private void whenCheckingRecipeListReturned() {
+        recipeListArgumentCapture = ArgumentCaptor.forClass(List.class);
+        verify(modelMock, times(1)).addAttribute(any(), recipeListArgumentCapture.capture());
+    }
+
+    private void thenAddAttributeForModelIsCalledOnce() {
         verify(modelMock, times(1)).addAttribute(any(), any());
     }
+
+    private void thenGetRecipesIsCalledOnce() {
+        verify(recipeServiceMock, times(1)).getRecipes();
+    }
+
+    private void thenReturnIndexViewName() {
+        assertEquals("index", viewName);
+    }
+
+    private void thenArgumentCaptureContainsTwoRecipes() {
+        List<Recipe> recipesInController = recipeListArgumentCapture.getValue();
+        assertEquals(2, recipesInController.size());
+    }
+
+
 }
