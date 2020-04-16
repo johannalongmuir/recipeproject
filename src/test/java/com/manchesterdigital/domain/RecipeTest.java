ecd6d5aa29f4565eea1b09209496f274afab8232
Johanna Longmuir <johannalongmuir@gmail.com>
2020-04-16 14:25:50 +0100

    Add empty test for Recipe to remind to complete

---
+++ src/test/java/com/manchesterdigital/domain/RecipeTest.java
@@ -0,0 +1,28 @@
+package com.manchesterdigital.domain;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class RecipeTest {
+
+    /*
+    Can use argument capture to check what was beforehand?
+    Then check there was a change?
+    e.g argument.capture on recipe
+    add ingredient.
+    then after add ingredient called. recipe has that ingredient?
+    Need to look over how method set up to work.
+     */
+    @Disabled(value = "Test for addIngredient To be implemented")
+    @Test
+    void addIngredient() {
+    }
+
+    @Disabled(value = "Test for setNotes To be implemented")
+    @Test
+    void setNotes() {
+    }
+}
