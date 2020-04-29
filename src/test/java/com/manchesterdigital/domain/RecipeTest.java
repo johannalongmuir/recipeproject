2c182a2f21d24c2c549f0341a787d33cb13da5ec
Johanna Longmuir <johannalongmuir@gmail.com>
2020-04-29 14:36:06 +0100

    Circle CI test for Java 8

--- src/test/java/com/manchesterdigital/domain/RecipeTest.java
+++ src/test/java/com/manchesterdigital/domain/RecipeTest.java
@@ -1,11 +1,5 @@
 package com.manchesterdigital.domain;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
 class RecipeTest {
 
     /*
@@ -16,13 +10,13 @@ class RecipeTest {
     then after add ingredient called. recipe has that ingredient?
     Need to look over how method set up to work.
      */
-    @Disabled(value = "Test for addIngredient To be implemented")
-    @Test
-    void addIngredient() {
-    }
-
-    @Disabled(value = "Test for setNotes To be implemented")
-    @Test
-    void setNotes() {
-    }
+//    @Disabled(value = "Test for addIngredient To be implemented")
+//    @Test
+//    void addIngredient() {
+//    }
+//
+//    @Disabled(value = "Test for setNotes To be implemented")
+//    @Test
+//    void setNotes() {
+//    }
 }
